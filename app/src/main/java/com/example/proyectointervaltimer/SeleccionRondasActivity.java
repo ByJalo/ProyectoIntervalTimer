30eda804694f50974d5f019bab3c800b346e5e2b
pinchiguillo <javiagua2@gmail.com>
2023-11-23 20:50:40 +0100

    TimeSelector FIX

--- app/src/main/java/com/example/proyectointervaltimer/SeleccionRondasActivity.java
+++ app/src/main/java/com/example/proyectointervaltimer/SeleccionRondasActivity.java
@@ -16,31 +16,61 @@ public class SeleccionRondasActivity extends AppCompatActivity {
         setContentView(R.layout.seleccion_rondas);
 
         SeekBar barraRondas = (SeekBar) findViewById(R.id.seekBarRondas);
-        barraRondas.setMax(30);
-        EditText rondas = (EditText) findViewById(R.id.Rondas);
+        barraRondas.setMax(10);
+
+        EditText textoRondas = (EditText) findViewById(R.id.Rondas);
+        textoRondas.setText("1");
 
         barraRondas.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            int rondas = 1;
+
             @Override
-            public void onProgressChanged(SeekBar bar, int i, boolean b) {
-                bar = barraRondas;
-                i = bar.getProgress();
-                rondas.setText(i);
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                rondas = progress;
+                textoRondas.setText(String.valueOf(rondas));
             }
+
             @Override
-            public void onStartTrackingTouch(SeekBar bar) {}
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                textoRondas.setText(String.valueOf(rondas));
+            }
+
             @Override
-            public void onStopTrackingTouch(SeekBar bar) {}
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                textoRondas.setText(String.valueOf(rondas));
+            }
         });
-        rondas.addTextChangedListener(new TextWatcher() {
-            public void afterTextChanged(Editable s) {
-                int i = Integer.parseInt(s.toString());
-                if (i<barraRondas.getMax() && i>0){
-                    barraRondas.setProgress(i);
+
+        textoRondas.addTextChangedListener(new TextWatcher() {
+            int rondas = 1;
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                try {
+                    rondas = Integer.parseInt(s.toString());
+                } catch (NumberFormatException e) {
+                    rondas = 1;
                 }
             }
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
 
-            public void onTextChanged(CharSequence s, int start, int before, int count) {}
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                try {
+                    rondas = Integer.parseInt(s.toString());
+                } catch (NumberFormatException e) {
+                    rondas = 1;
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                try {
+                    rondas = Integer.parseInt(s.toString());
+                } catch (NumberFormatException e) {
+                    rondas = 1;
+                }
+                barraRondas.setProgress(rondas);
+            }
         });
 
     }
