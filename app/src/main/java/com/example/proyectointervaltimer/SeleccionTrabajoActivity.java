c5313f630a2d00b639d3d40eb5788675a422184f
Adrian <adri.go2004@gmail.com>
2023-11-23 18:26:47 +0100

    Primer Indent

---
+++ app/src/main/java/com/example/proyectointervaltimer/SeleccionTrabajoActivity.java
@@ -0,0 +1,13 @@
+package com.example.proyectointervaltimer;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SeleccionTrabajoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.seleccion_trabajo);
+    }
+}
