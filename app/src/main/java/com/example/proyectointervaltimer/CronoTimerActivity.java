2782848c654b8dbc68445fbc1b674855a7658a77
Adrian <adri.go2004@gmail.com>
2023-12-13 11:00:22 +0100

    CronoTimer

--- app/src/main/java/com/example/proyectointervaltimer/CronoTimerActivity.java
+++ app/src/main/java/com/example/proyectointervaltimer/CronoTimerActivity.java
@@ -1,55 +1,41 @@
 package com.example.proyectointervaltimer;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.os.CountDownTimer;
+import android.os.Handler;
+import android.os.SystemClock;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 
-public class CronoTimerActivity extends AppCompatActivity {
-    private Button startButton;
-    private ProgressBar progressBar;
-    private CountDownTimer countDownTimer;
-    private TextView textViewTimer;
-    private Button buttonStart, buttonStop;
+import androidx.appcompat.app.AppCompatActivity;
 
-    private long timeLeftInMillis;
+public class CronoTimerActivity extends AppCompatActivity {
+    private Button btnStartPause, btnReset;
+    private TextView tvTimer;
+
+    private Handler handler;
     private boolean timerRunning;
+    private long startTime, elapsedTime;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.crono_timer);
 
-        startButton = findViewById(R.id.buttonStart);
-        progressBar = findViewById(R.id.progressBar);
-        textViewTimer = findViewById(R.id.tiempo);
-        buttonStart = findViewById(R.id.buttonStart);
-        buttonStop = findViewById(R.id.buttonStop);
+        btnStartPause = findViewById(R.id.btnStartPause);
+        btnReset = findViewById(R.id.btnReset);
+        tvTimer = findViewById(R.id.tvTimer);
 
-        startButton.setOnClickListener(new View.OnClickListener() {
+        handler = new Handler();
+
+        btnStartPause.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                int work_time = getIntent().getIntExtra("work_time", 60);
-                int timeout = getIntent().getIntExtra("timeout", 15);
-                int iterations = getIntent().getIntExtra("iterations", 3);
-                int rounds = getIntent().getIntExtra("rounds", 1);
-                int round_reset = getIntent().getIntExtra("round_reset", 0);
-
-                start(work_time, timeout, iterations, rounds, round_reset);
-                startStopTimer();
+                startPauseTimer();
             }
         });
-        buttonStart.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startStopTimer();
-                updateCountDownText();
-            }
-        });
-        buttonStop.setOnClickListener(new View.OnClickListener() {
+
+        btnReset.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 resetTimer();
@@ -57,95 +43,52 @@ public class CronoTimerActivity extends AppCompatActivity {
         });
     }
 
-    private void start(int work_time, int timeout, int iterations, int rounds, int round_reset) {
-
-        for (int i = 0; i < rounds; i++) {
-            for (int j = 0; j < iterations; j++) {
-                startCountdown();
-                startStopTimer();
-
-            }
-            if (round_reset > 0) {
-                startCountdown();
-                startStopTimer();
-            }
-        }
-
-    }
-
-    private void updateCountDownText() {
-        int hours = (int) (timeLeftInMillis / 1000) / 3600;
-        int minutes = (int) ((timeLeftInMillis / 1000) % 3600) / 60;
-        int seconds = (int) (timeLeftInMillis / 1000) % 60;
-
-        String timeLeftFormatted = String.format("%02d:%02d:%02d", hours, minutes, seconds);
-        textViewTimer.setText(timeLeftFormatted);
-    }
-    private void resetTimer() {
-        timeLeftInMillis = 0;
-        updateCountDownText();
-        buttonStop.setVisibility(View.INVISIBLE);
-        buttonStart.setVisibility(View.VISIBLE);
-    }
-    private void pauseTimer() {
-        countDownTimer.cancel();
-        timerRunning = false;
-        buttonStart.setText("Comenzar");
-        buttonStop.setVisibility(View.VISIBLE);
-    }
-    private void startTimer() {
-        countDownTimer = new CountDownTimer(timeLeftInMillis, 1000) {
-            @Override
-            public void onTick(long millisUntilFinished) {
-                timeLeftInMillis = millisUntilFinished;
-                updateCountDownText();
-            }
-
-            @Override
-            public void onFinish() {
-                timerRunning = false;
-                buttonStart.setText("Comenzar");
-                buttonStop.setVisibility(View.INVISIBLE);
-            }
-        }.start();
-
-        timerRunning = true;
-        buttonStart.setText("Pausa");
-        buttonStop.setVisibility(View.VISIBLE);
-    }
-
-    private void startStopTimer() {
+    private void startPauseTimer() {
         if (timerRunning) {
             pauseTimer();
-            startCountdown();
         } else {
             startTimer();
-            onDestroy();
         }
     }
-    private void startCountdown() {
-        countDownTimer = new CountDownTimer(10000, 100) {
-            @Override
-            public void onTick(long millisUntilFinished) {
-                int progress = (int) (millisUntilFinished / 100); // Actualizar la ProgressBar
-                progressBar.setProgress(progress);
-            }
 
+    private void startTimer() {
+        startTime = SystemClock.uptimeMillis() - elapsedTime;
+
+        handler.postDelayed(new Runnable() {
             @Override
-            public void onFinish() {
-                progressBar.setProgress(0);
-                // Activar la cuenta u realizar otras acciones al finalizar el conteo.
+            public void run() {
+                elapsedTime = SystemClock.uptimeMillis() - startTime;
+                updateCountUpText(elapsedTime);
+                handler.postDelayed(this, 1000);
             }
-        };
-        countDownTimer.start();
+        }, 1000);
+
+        timerRunning = true;
+        btnStartPause.setText("Pause");
     }
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        if (countDownTimer != null) {
-            countDownTimer.cancel();
-        }
+    private void pauseTimer() {
+        handler.removeCallbacksAndMessages(null);
+        timerRunning = false;
+        btnStartPause.setText("Start");
+    }
+
+    private void resetTimer() {
+        handler.removeCallbacksAndMessages(null);
+        timerRunning = false;
+        btnStartPause.setText("Start");
+        elapsedTime = 0;
+        updateCountUpText(elapsedTime);
+    }
+
+    private void updateCountUpText(long elapsedTime) {
+        int seconds = (int) (elapsedTime / 1000);
+        int minutes = seconds / 60;
+        seconds = seconds % 60;
+        int hours = minutes / 60;
+        minutes = minutes % 60;
+
+        String timeFormatted = String.format("%02d:%02d:%02d", hours, minutes, seconds);
+        tvTimer.setText(timeFormatted);
     }
 }
-
